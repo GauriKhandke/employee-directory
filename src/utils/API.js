1252ee34ee34657aab042effbf791e39e22c1d8f
Gauri Khandke <khandkegauri@gmail.com>
2020-11-01 17:53:44 -0800

    Updated api url for 30 users

--- src/utils/API.js
+++ src/utils/API.js
@@ -5,7 +5,7 @@ const API =  {
   
   // Gets all users
   getUsers: function() {
-    return axios.get('https://randomuser.me/api/?results=200&nat=us');
+    return axios.get('https://randomuser.me/api/?results=30&nat=us');
   }
 };
 
